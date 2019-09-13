acfc7a2614415921ecfee03da061a1de15a3d4c3
AbnerAbreu <abnerdrift94@gmail.com>
2019-09-13 17:51:00 -0300

    Adicionar relacionamento/artista

--- artista/serializers.py
+++ artista/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
 from artista.models import Artista
 
+from musica.models import Musica
 
-class ArtistaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Artista
-        fields =('id','nome', 'idade', 'estilo_musical')
+class ArtistaSerializer(serializers.Serializer):
+    id = serializers.intergeField()
+    nome = serializers.Charfield(read_only=True)
+
+class MusicaSerializer(serializers.Serializer):
+    id = serializers.intergeField()
+    nome = serializers.Charfield(max_length=255,required=True)
+    idade = serializers.intergeField()
+    estilo_musical = serializers.Charfield()
+
+    def create(self, validated_data):
+        musica_data = validated_data.pop('estilo_musical')
+        musica = Musica.objects.get(id=musica_data['id'])
+        artista = Artista.objects.create(estilo_musical=Musica, **validated_data)
+        return artista
+
+    def update(self, instance, validated_data):
+        instance.nome = validated_data.get('nome')
+        instance.idade = validated_data.get('idade')
+        instance.email = validated_data.get('estilo_musical')
+        musica = Musica.objects.get(id=musica_data['id'])
+        instance.estilo_musical = musica
+        instance.save()
+        return instance
+
+
+class ArtistaLightSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    nome = serializers.CharField(max_length=255)
