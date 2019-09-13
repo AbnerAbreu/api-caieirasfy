a3d65bd957de0534c1b813493f83413886e9e873
matheus s. silva <viciogamer157@gmail.com>
2019-09-13 16:42:32 -0300

    adicionar e fazer views

--- musica/views.py
+++ musica/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from musica.models import Musica
+from musica.serializers import MusicaSerializer
+
+
+class MusicaViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = MusicaSerializer
+    queryset = Musica.objects.all()
+    search_fields = ['^nome', '@tempo_reprodução', '=genero', '^artista']
+    filter_backends = [SearchFilter]
